ce0ad677d1f366435a3bc40bfc0b0ea39924da88
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-03-04 10:26:07 -0600

    add 9 buttons to launch view

--- app/src/main/java/com/hci2_grp6/app/LaunchActivity.java
+++ app/src/main/java/com/hci2_grp6/app/LaunchActivity.java
@@ -107,11 +107,6 @@ public class LaunchActivity extends Activity {
                 }
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
