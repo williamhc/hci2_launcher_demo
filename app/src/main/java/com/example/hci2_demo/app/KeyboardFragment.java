b661aadf4ce7d66dc8ab32d0520775af8f278d53
Cameron McKay <cameron0960@gmail.com>
2014-03-16 15:03:15 -0500

    Added keyboard fragment

---
+++ app/src/main/java/com/example/hci2_demo/app/KeyboardFragment.java
@@ -0,0 +1,47 @@
+package com.example.hci2_demo.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+
+import experiment.Trial;
+
+public class KeyboardFragment extends LauncherFragment {
+    private Trial trial;
+    private Activity activity;
+
+    public KeyboardFragment(Context context, Trial trial) {
+        super(context);
+        this.activity = this.getActivity();
+        this.trial = trial;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(this.getLayoutID(), container, false);
+        Button keyboardBtn = (Button) rootView.findViewById(R.id.keyboard);
+
+        View.OnClickListener keyboardListener = new View.OnClickListener() {
+            public void onClick(View view) {
+                InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+            }
+        };
+
+        keyboardBtn.setOnClickListener(keyboardListener);
+
+        return rootView;
+    }
+
+    public int getLayoutID() {
+        return R.layout.keyboard_search;
+    }
+
+
+}
